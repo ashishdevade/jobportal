df7fee2bfd50f4de2485e83670e91bb9fd24c881
Meenesh Jain <meenesh.jain@mappes.io>
2021-08-29 16:54:30 +0530

    fine tune job posting list

--- src/app/modules/users/dashboard/dashboard.component.ts
+++ src/app/modules/users/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild  } from '@angular/core';
+import { Router, ActivatedRoute , NavigationEnd } from '@angular/router';
+
+import { CommonFunctions } from "src/app/core/helpers/common.functions";
+import { CommonService } from "src/app/core/services/common.service";
+import { MainService } from "src/app/core/services/main.service";
+import { SharedService } from "src/app/core/services/shared.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +12,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  
+  public common_params = new CommonFunctions();
+  public show_loader = false;
+  public form_data:any = {};
+  public category_list = [];
+  public Industry_list = [];
+  public skill_list = [];
+  
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private ActivatedRoute: ActivatedRoute,
+    public common_service : CommonService,
+    public service : MainService,
+    /*  private modalService: BsModalService,*/
+    public shared_service : SharedService
+    ) {  
+    
+    this.common_service.check_admin_session_on();
+  }
 
   ngOnInit() {
+    
+    this.category_list = [{"id":"1","name":"test"}, {"id":"2","name":"test 2"}, {"id":"3","name":"test 3"}];
+    this.Industry_list = [{"id":"1","name":"test"}, {"id":"2","name":"test 2"}, {"id":"3","name":"test 3"}];
+    this.skill_list = [{"id":"1","name":"test"}, {"id":"2","name":"test 2"}, {"id":"3","name":"test 3"}];
   }
+  
+  get_category($event){
+    this.form_data.country_name = $event.name;
+    this.form_data.country_id = $event.id;
+    
+  }
+  
+  get_industry($event){
+    this.form_data.country_name = $event.name;
+    this.form_data.country_id = $event.id;
+    
+  }
+  
+  get_skills($event){
+    this.form_data.country_name = $event.name;
+    this.form_data.country_id = $event.id;
+    
+  }
+
 
 }
--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { NgSelectModule } from '@ng-select/ng-select'; 
+import { SharedModules } from '../../core/shared.module';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -7,8 +11,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
-    CommonModule,
-    UsersRoutingModule
+  CommonModule,
+  UsersRoutingModule,
+  CommonModule,
+  SharedModules,
+  ModalModule.forRoot(),
+  BsDatepickerModule.forRoot(),
+  NgSelectModule,
   ]
 })
 export class UsersModule { }
